f0d1ea6ae85a88f63ad1dda0a174f518c503739b
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2023-10-02 07:58:13 -0500

    feat(Metrics): add a chart to show count of wealth and no wealth agents
    Now we can visualize the number of agents Wealthy and No Wealthy. Also we can modify the agents amount with an slider

--- money-wealth-simulation/money_model.py
+++ money-wealth-simulation/money_model.py
@@ -1,4 +1,4 @@
-from mesa import Model
+from mesa import Model, DataCollector
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from money_agent import MoneyAgent
@@ -9,6 +9,13 @@ class MoneyModel(Model):
         self.agents_amount = agents_amount
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
+        self.running = True
+        self.datacollector = DataCollector(
+            model_reporters={
+                "Wealthy": self.count_wealthy_agents,
+                "No Wealthy": self.count_no_wealthy_agents,
+            },
+        )
 
         for i in range(self.agents_amount):
             newAgent = MoneyAgent(i, self)
@@ -20,3 +27,10 @@ class MoneyModel(Model):
 
     def step(self) -> None:
         self.schedule.step()
+        self.datacollector.collect(self)
+
+    def count_wealthy_agents(self) -> int:
+        return sum([1 for a in self.schedule.agents if a.wealth > 0])
+
+    def count_no_wealthy_agents(self) -> int:
+        return sum([1 for a in self.schedule.agents if a.wealth == 0])
